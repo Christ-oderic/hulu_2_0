4af283df92e39f03cf71969b9ff2e953d02e1675
christ-oderic <christyassinguezo@gmail.com>
2023-12-20 18:11:59 +0100

    création du component headerItem

---
+++ src/components/HeaderItem.vue
@@ -0,0 +1,16 @@
+<template>    
+    <li class="flex flex-col items-center cursor-pointer w-20 sm:w-20 hover:text-white group">
+        <Icon class="h-8 mb-1 group-hover:animate-bounce" />
+        <a href="#" class="uppercase">{{title}}</a>
+    </li>
+</template>
+
+<script setup lang="ts">
+import type { FunctionalComponent, HTMLAttributes, VNodeProps } from 'vue';
+
+const props = defineProps<{
+    title: String,
+    Icon: FunctionalComponent<HTMLAttributes & VNodeProps, {}, any>,
+}>();
+
+</script>
