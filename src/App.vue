2ee574458cbb10f4cfc36033a6487903c3245f0a
christ-oderic <christyassinguezo@gmail.com>
2023-12-21 12:00:54 +0100

    importation de la navbar dans la vue principal

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <HeaderComp />
   <main>
-
+    <NavbarComp />
   </main>
 </template>
 
 <script setup lang="ts">
 import HeaderComp  from "@/components/HeaderComp.vue"
+import NavbarComp  from "@/components/NavbarComp.vue"
 
 
 </script>
