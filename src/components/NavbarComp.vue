dbd7f3faffd1bd85780c617fc82d9c9512d2ac92
christ-oderic <christyassinguezo@gmail.com>
2023-12-21 14:53:06 +0100

    mise en place du scrollbar-hide au niveau de la navbar

--- src/components/NavbarComp.vue
+++ src/components/NavbarComp.vue
@@ -1,6 +1,20 @@
 <template>
-    <div>
-        <h1>Navbar</h1>
-    </div>
+    <nav>
+        <ul class="flex px-10 sm:px-20 text-2xl whitespace-nowrap gap-10 sm:gap-20 overflow-x-scroll scrollbar-hide">
+            <li v-for="title of genreList" :key="title" class="transition duration-300 hover:scale-125 hover:text-white active:text-red-500 ">
+                <button> {{title}} </button>
+            </li>
+        </ul>
+    </nav>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+    import request from "@/utils/request.ts"
+
+    const genreList: string[] = [];
+
+    Object.entries(request).forEach(([key, {title, url}]) => {
+        genreList.push(title);       
+    })
+    console.log(request);
+    
+</script>
